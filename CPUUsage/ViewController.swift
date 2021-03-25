c6e8d2ccf4c23538e8d1a80a095422d60a8b2f77
sharathujiredatamatics <sharath.ujire@digital.datamatics.com>
2021-03-25 15:50:17 +0530

    Initial Commit

---
+++ CPUUsage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CPUUsage
+//
+//  Created by Datamatics on 25/03/21.
+//  Copyright © 2021 Datamatics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
