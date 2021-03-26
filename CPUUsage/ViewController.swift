d03e66c837f0d46a28a4c7bc34b7cd495eca27ea
sharathujiredatamatics <sharath.ujire@digital.datamatics.com>
2021-03-26 23:03:15 +0530

    25th March

--- CPUUsage/ViewController.swift
+++ CPUUsage/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var statusLabel: UILabel!
+    var student : Student?
+    var laptop : Laptop?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func checkStatus(_ sender: UIButton) {
+        student = Student(name: "Gopal", laptop: nil)
+        laptop = Laptop(name: "HP", belongsTo: nil)
+        assignProperty()
+        //        student = nil
+        //        laptop = nil
+        
+    }
+    func assignProperty(){
+        student?.laptop = laptop
+        laptop?.belongsTo = student
+        student = nil
+        laptop = nil
+    }
 }
 
+class Student{
+    let name : String
+    // Making weak property
+    weak var laptop : Laptop?
+    //    let laptop : Laptop?
+    init(name: String, laptop : Laptop?) {
+        self.name = name
+        self.laptop = laptop
+    }
+    deinit {
+        print("\(name) is deinitilized")
+    }
+}
+class Laptop{
+    let name : String
+    var belongsTo : Student?
+    //    let belongsTo : Student?
+    init(name: String, belongsTo : Student?) {
+        self.name = name
+        self.belongsTo = belongsTo
+    }
+    deinit {
+        print("\(name) is deinitilized")
+    }
+}
